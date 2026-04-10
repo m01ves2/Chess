3a381b0f7e9fced1293a737c213844afdd5f7fad
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2026-04-10 06:28:59 +0300

    Feat: MoveRater optimizations - bonuses for promotional moves, bonuses for center controlling, etc

--- Chess.Application/Players/AI/MoveSearch.cs
+++ Chess.Application/Players/AI/MoveSearch.cs
@@ -3,6 +3,7 @@ using Chess.Domain;
 using Chess.Domain.Moves;
 using Chess.Domain.Pieces;
 using Chess.Engine;
+using System.IO;
 
 namespace Chess.Application.Players.AI
 {
@@ -13,11 +14,11 @@ namespace Chess.Application.Players.AI
         private readonly PieceColor _aiColor;
         private readonly GameEngine _engine = new GameEngine();
         private static readonly Random _random = new Random();
-        Dictionary<string, int> _cache = new();
+        private Dictionary<string, int> _cache = new(); //кеш для рассчета позиций, которые уже встречались
 
         int _ttHits = 0;
         int _ttMisses = 0;
-        int totalMovesProcessed = 0;
+        int _totalMovesProcessed = 0;
 
         public MoveSearch(GameController gameController, int aiDifficulty, PieceColor aiColor)
         {
@@ -35,7 +36,7 @@ namespace Chess.Application.Players.AI
             int bestScore = int.MinValue;
             List<Move> bestMoves = new List<Move>();
             moves = moves.OrderByDescending(move => MoveRater.ScoreMove(_gameController.GamePosition, move)).ToList();
-            totalMovesProcessed += moves.Count();
+            _totalMovesProcessed += moves.Count();
             foreach (var move in moves) {
 
                 if(move is PromotionMove) {
@@ -47,7 +48,9 @@ namespace Chess.Application.Players.AI
                 simEngine.MakeMove(simPosition, move);
                 simPosition.SwitchTurn(); // переключаем ход
 
-                int score = Minimax(simPosition, _depth, int.MinValue, int.MaxValue);
+                HashSet<string> path = new HashSet<string>(); //путь для выявления циклов в принятии решений Ai, чтобы их избегать.
+
+                int score = Minimax(simPosition, _depth, int.MinValue, int.MaxValue, path);
 
                 if (score > bestScore) {
                     bestScore = score;
@@ -62,18 +65,19 @@ namespace Chess.Application.Players.AI
             if (bestMoves.Count == 0)
                 return null;
 
-            System.Diagnostics.Debug.WriteLine($"_ttHits: {_ttHits}, _ttMisses: {_ttMisses}, 100%*_ttHits / (_ttHits + _ttMisses): {100 * (float)_ttHits / (_ttHits + _ttMisses)} ");
-            System.Diagnostics.Debug.WriteLine($"totalMovesProcessed: {totalMovesProcessed}");
-            totalMovesProcessed = 0;
+            //System.Diagnostics.Debug.WriteLine($"_ttHits: {_ttHits}, _ttMisses: {_ttMisses}, 100%*_ttHits / (_ttHits + _ttMisses): {100 * (float)_ttHits / (_ttHits + _ttMisses)} ");
+            //System.Diagnostics.Debug.WriteLine($"totalMovesProcessed: {totalMovesProcessed}");
+            //totalMovesProcessed = 0;
 
             // Выбираем случайный ход среди лучших
             return bestMoves[_random.Next(bestMoves.Count)];
         }
 
-        private int Minimax(GamePosition position, int depth, int alpha, int beta)
+        private int Minimax(GamePosition position, int depth, int alpha, int beta, HashSet<string> path)
         {
             if (depth == 0)
                 return Evaluator.Evaluate(position, _aiColor);
+                //return Quiescence(position, alpha, beta);
 
             var key = TranspositionTable.GetKey(position);
             if (_cache.TryGetValue(key, out var cached)) {
@@ -84,9 +88,14 @@ namespace Chess.Application.Players.AI
                 _ttMisses++;
             }
 
+            if (path.Contains(key)) { //отслеживание позиций, которые уже встречались
+                return 0; // ничья / повтор
+            }
+            path.Add(key); //зафикцировали то, что уже встречали позицию
+
             var currentColor = position.CurrentPlayerColor;
             var moves = _engine.GetAllPseudoMoves(position, currentColor).OrderByDescending(m => MoveRater.ScoreMove(position, m)).ToList();
-            totalMovesProcessed += moves.Count();
+            _totalMovesProcessed += moves.Count();
 
             if (!moves.Any()) {
                 if (_engine.IsKingInCheck(position, currentColor))
@@ -105,14 +114,13 @@ namespace Chess.Application.Players.AI
                 }
 
                 var simPosition = position.Clone();       // клон всей позиции
-                var simEngine = new GameEngine();
-                simEngine.MakeMove(simPosition, move);
+                _engine.MakeMove(simPosition, move);
                 if (_engine.IsKingInCheck(simPosition, currentColor))
                     continue;
 
                 simPosition.SwitchTurn();                 // переключаем ход
 
-                int eval = Minimax(simPosition, depth - 1, alpha, beta);
+                int eval = Minimax(simPosition, depth - 1, alpha, beta, path);
 
                 if (isMaximizing) {
                     bestEval = Math.Max(bestEval, eval);
@@ -129,8 +137,9 @@ namespace Chess.Application.Players.AI
                     break;
                 }
             }
+            path.Remove(key); //удаляем из сохранённых позиций
 
-            //if (!cutoff)
+            if (!cutoff)
                 _cache[key] = bestEval;
             return bestEval;
         }
@@ -141,10 +150,49 @@ namespace Chess.Application.Players.AI
             //    move = new PromotionMove(pm.From, pm.To, pm.Piece, pm.CapturedPiece);
             //    ((PromotionMove)move).SetPromotionPiece(new Queen(pm.Piece.Color));
             //}
-
             if (move is PromotionMove pm && pm.PromotionPiece == null) {
                 pm.SetPromotionPiece(new Queen(pm.Piece.Color));
             }
+        }
+
+        private int Quiescence(GamePosition position, int alpha, int beta)
+        {
+            int standPat = Evaluator.Evaluate(position, _aiColor);
+
+            if (standPat >= beta)
+                return beta;
+
+            if (standPat > alpha)
+                alpha = standPat;
+
+            var currentColor = position.CurrentPlayerColor;
+
+            // только ВЗЯТИЯ
+            var moves = _engine.GetAllPseudoMoves(position, currentColor).Where(m => m.CapturedPiece != null);
+
+            foreach (var move in moves) {
+                if (move is PromotionMove) {
+                    PromotionMoveCasting(move);
+                }
+
+                var simPosition = position.Clone();
+                _engine.MakeMove(simPosition, move);
+
+                if (_engine.IsKingInCheck(simPosition, currentColor))
+                    continue;
+
+                simPosition.SwitchTurn();
+
+                int score = -Quiescence(simPosition, -beta, -alpha); // negamax стиль
+
+                if (score >= beta)
+                    return beta;
+
+                if (score > alpha)
+                    alpha = score;
+            }
+
+            return alpha;
         }
     }
 }
--- Chess.Application/Players/AI/Optimizers/MoveRater.cs
+++ Chess.Application/Players/AI/Optimizers/MoveRater.cs
@@ -14,13 +14,15 @@ namespace Chess.Application.Players.AI.Optimizers
 
             // 1. Capture (MVV-LVA)
             if (move.CapturedPiece != null) {
-                score += GetPieceWeight(move.CapturedPiece) * 10
-                       - GetPieceWeight(move.Piece);
+                score += GetPieceWeight(move.CapturedPiece) * 10 - GetPieceWeight(move.Piece);
             }
 
             // 2. Promotion
             if (move is PromotionMove)
                 score += 100;
+
+            if (move is KingCastlingMove)
+                score += 50;
 
             // 3. Checks (да, через clone пока)
             if (GivesCheck(position, move))
@@ -28,14 +30,19 @@ namespace Chess.Application.Players.AI.Optimizers
 
             // 4. Piece activity
             if (move.Piece is Knight or Bishop)
-                score += 2;
+                score += 10;
 
             if (move.Piece is Queen)
-                score -= 1;
+                score -= 5;
 
             // 5. Pawn penalty
             if (move.Piece is Pawn && move.CapturedPiece == null)
                 score -= 1;
+
+            // 6. Move piece to the center
+            if (IsCenter4x4(move.To)) { //если фигуры Ai толпятся в центре - они больше контролируют пространства. поощряем
+                score += 1;
+            }
 
             return score;
         }
@@ -53,13 +60,47 @@ namespace Chess.Application.Players.AI.Optimizers
 
         private static int GetPieceWeight(Piece piece) => piece switch
         {
-            Pawn => 1,
-            Knight => 3,
-            Bishop => 3,
-            Rook => 5,
-            Queen => 9,
-            King => 8,
+            Pawn => 2,
+            Knight => 5,
+            Bishop => 5,
+            Rook => 10,
+            Queen => 50,
+            King => 40,
             _ => 0
         };
+
+        private static bool IsCenter4x4(Position pos)
+        {
+            return pos.Row >= 2 && pos.Row <= 5 &&
+                   pos.Col >= 2 && pos.Col <= 5;
+        }
+
+        private static int ScoresForPromotionalMove(Move move)
+        {
+            var score = 0;
+            var piece = move.Piece;
+
+            if (piece is Knight && !((move.From.Row == 0 && (move.From.Col == 1 || move.From.Col == 6) && piece.Color == PieceColor.Black) ||
+                                     (move.From.Row == 7 && (move.From.Col == 1 || move.From.Col == 6) && piece.Color == PieceColor.White)))
+                score += 1;
+
+            if (piece is Bishop && !((move.From.Row == 0 && (move.From.Col == 2 || move.From.Col == 5) && piece.Color == PieceColor.Black) ||
+                                     (move.From.Row == 7 && (move.From.Col == 2 || move.From.Col == 5) && piece.Color == PieceColor.White)))
+                score += 1;
+
+            if (piece is Rook   && !((move.From.Row == 0 && (move.From.Col == 0 || move.From.Col == 7) && piece.Color == PieceColor.Black) ||
+                                     (move.From.Row == 7 && (move.From.Col == 0 || move.From.Col == 7) && piece.Color == PieceColor.White)))
+                score += 1;
+
+            if (piece is Queen &&  !((move.From.Row == 0 && move.From.Col == 3 && piece.Color == PieceColor.Black) ||
+                                     (move.From.Row == 7 && move.From.Col == 3 && piece.Color == PieceColor.White)))
+                score += 1;
+
+            if (piece is Pawn && !((move.From.Row == 1 && piece.Color == PieceColor.Black) ||
+                                     (move.From.Row == 6 && piece.Color == PieceColor.White)))
+                score += 1;
+
+            return score;
+        }
     }
 }
